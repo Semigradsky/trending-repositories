bb715e9cbfe76718b83b544513900d2175cfd92d
Semigradsky <d.semigradsky@itransition.com>
2015-05-18 11:39:03 +0300

    Fix spaces

--- generateMd.js
+++ generateMd.js
@@ -13,11 +13,11 @@ var repos = JSON.parse(fs.readFileSync('list.json', { encoding: 'UTF-8' }));
 
 repos.forEach(function (repo) {
 	content +=
-		'| ' + (repo.stars > 10000 ? '<img alt=":cake:" src="' + starImg + '" width="20">' : 
+		'| ' + (repo.stars > 10000 ? '<img alt=":cake:" src="' + starImg + '" width="20">' :
 			(repo.stars > 5000 ? '<img alt=":cake:" src="' + cakeImg + '" width="20">' : '')) +
 		'| [<img src="{avatar}" alt="owwner" width="50">]({ownerUrl}) '.replace('{avatar}', repo.avatar).replace('{ownerUrl}', repo.ownerUrl) +
 		'| <div>[{name}]({url})</div> '.replace('{name}', repo.name).replace('{url}', repo.url) +
-		' {description} '.replace('{description}', repo.description.replace(/\|.*/, '')) +
+		' {description} '.replace('{description}', repo.description.replace(/\|.*/g, '').replace(/\s+/g, ' ')) +
 		(repo.homepage ? '| [<img src="' + homeImg + '" width="30" alt="home">]({homepage})'.replace('{homepage}', repo.homepage) : '| ') +
 		'| {stars} '.replace('{stars}', humanize(repo.stars)) +
 		(repo.language ? '| *{language}*\n'.replace('{language}', repo.language) : '| \n');
